3b0e7a1804ca6b1929b40962647725c14ca6ce1f
Antonella Schiavoni <antonellaschiavoni2@gmail.com>
2022-09-29 09:13:19 +0200

    adapt script to fit our needs

--- train_and_deploy/build_and_push.sh
+++ train_and_deploy/build_and_push.sh
@@ -27,32 +27,30 @@ fi
 #if you only want to specify one region, use region, otherwise, use regions
 region=$(aws configure get region)
 
-if [[ $region =~ ^cn.* ]]
-then
-d
-elif [[ $region = "ap-east-1" ]]
-then
-    fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
-    registry_id="871362719292"
-    registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com"
-else
-    fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
-    registry_id="763104351884"
-    registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com"
-fi
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+registry_id="239350501239"
+registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com"
 
 echo ${fullname}
+echo ${registry_uri}
+echo ${region}
 
 # Get the login command from ECR and execute it directly
-$(aws ecr get-login --registry-ids ${account} --region ${region} --no-include-email)
-$(aws ecr get-login --registry-ids ${registry_id} --region ${region} --no-include-email)
+
+
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+
+#aws ecr get-login-password --region ${region} --registry-ids 239350501239 | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${image}" --region ${region} || aws ecr create-repository --repository-name "${image}" --region ${region}
+aws ecr describe-repositories --repository-names "${image}" --region ${region}
+
+#|| aws ecr create-repository --repository-name "${image}" --region ${region}
 
 
 # Build the docker image, tag with full name and then push it to ECR
-#docker build -t ${image} -f Dockerfile . --build-arg REGISTRY_URI=${registry_uri}
-docker build -t ${image} -f Dockerfile . 
+docker build -t ${image} -f Dockerfile . --build-arg REGISTRY_URI=${registry_uri}
+#docker build -t ${image} -f Dockerfile .
 docker tag ${image} ${fullname}
 docker push ${fullname}
